b3a6e1ec4e82fd99138f845676d0c33372c3913d
Shichao An <shichao.an@nyu.edu>
2013-12-24 11:14:20 -0500

    naming to meter

--- soundmeter/meter.py
+++ soundmeter/meter.py
@@ -22,7 +22,7 @@ from .settings import (FRAMES_PER_BUFFER, FORMAT, CHANNELS, RATE,
 output = StringIO()
 
 
-def monitor():
+def meter():
     p = pyaudio.PyAudio()
     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True,
                     frames_per_buffer=FRAMES_PER_BUFFER)
@@ -50,7 +50,7 @@ def monitor():
 
 def main():
     while True:
-        monitor()
+        meter()
 
 
 def sigint_handler(signum, frame):
