4dda60fcefb98dbdb24dbc70ff50b19f74b45496
Shichao An <shichao.an@nyu.edu>
2013-12-27 22:42:35 -0500

    Sample monitor usage

--- soundmeter/monitor.py
+++ soundmeter/monitor.py
@@ -2,13 +2,21 @@ from .meter import Meter
 
 
 class Monitor(Meter):
+
     def __init__(self, *args, **kwargs):
         super(Monitor, self).__init__(*args, **kwargs)
 
     def monitor(self, rms):
-        """Extra monitor actions with RMS values"""
+        """Extra monitor actions with RMS values
+
+        Custom trigger conditions and actions can be written here as long as
+        `action=None` is passed to the Monitor(). The Monitor instance will
+        only execute monitor() as action is disabled.
+        """
         pass
 
+    # The following hooks can be used with as long as
+    # `action=None` is passed to the Monitor()
     def prepopen(self):
         """Extra code before executing the script"""
         pass
@@ -17,6 +25,7 @@ class Monitor(Meter):
         """Extra code after executing the script"""
         pass
 
+    # The following hooks can be used in all cases
     def prestop(self):
         """Extra code before stop"""
         pass
