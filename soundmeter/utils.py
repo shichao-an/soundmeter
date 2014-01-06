0e40a2e6e40652754083531f09552b867bea7968
Shichao An <shichao.an@nyu.edu>
2014-01-06 03:23:24 -0500

    No libasound.so on OS X

--- soundmeter/utils.py
+++ soundmeter/utils.py
@@ -34,7 +34,10 @@ c_error_handler = ERROR_HANDLER_FUNC(py_error_handler)
 
 @contextmanager
 def noalsaerr():
-    asound = cdll.LoadLibrary('libasound.so')
-    asound.snd_lib_error_set_handler(c_error_handler)
-    yield
-    asound.snd_lib_error_set_handler(None)
+    try:
+        asound = cdll.LoadLibrary('libasound.so')
+        asound.snd_lib_error_set_handler(c_error_handler)
+        yield
+        asound.snd_lib_error_set_handler(None)
+    except:
+        yield
