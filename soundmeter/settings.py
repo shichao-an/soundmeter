c827ba1ec1846847e44416c6ec5a74418558657c
Shichao An <shichao.an@nyu.edu>
2014-01-10 22:16:59 -0500

    Modify exception handling to local config names

--- soundmeter/settings.py
+++ soundmeter/settings.py
@@ -19,10 +19,13 @@ if config.has_section(PROG):
         try:
             if name in ['frames_per_buffer', 'format', 'channels', 'rate']:
                 items[name] = int(items[name])
-            if name in ['audio_segment_length']:
+            elif name in ['audio_segment_length']:
                 items[name] = float(items[name])
-        except:
-            items[name] = None
+            else:
+                raise Exception('Unknown name "%s" in config' % name)
+        except ValueError:
+            raise Exception('Invalid value to "%s" in config' % name)
+
 FRAMES_PER_BUFFER = items.get('frames_per_buffer') or 2048
 FORMAT = items.get('format') or pyaudio.paInt16
 CHANNELS = items.get('channels') or 2
